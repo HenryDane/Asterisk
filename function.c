e35da93bb4840816d8472a0f44d3cc1082609ced
HenryDane <aglareb7@gmail.com>
2018-04-27 18:03:07 -0700

    added functions files

---
+++ function.c
@@ -0,0 +1,16 @@
+void handle_quest_entities(int type){
+    switch (type){
+        default:
+            return;
+    }
+}
+
+bool q_act_0(){
+    printf("Acted with q_act_0() \n");
+    return true;
+}
+
+bool q_val_0(){
+    printf("Validated with q_val_0() \n");
+    return true;
+}
---
+++ function.h
@@ -0,0 +1,7 @@
+void handle_quest_entities(int type);
+
+// action functions
+bool q_act_0();
+
+// validation functions
+bool q_val_0();
